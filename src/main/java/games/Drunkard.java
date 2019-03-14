b81509b08e80e105d658f0268d01fc10ced0c422
Mikhail Kudrinskiy <m.kudrinskiy@gmail.com>
2019-03-14 20:08:43 +0300

    процесс шага 5

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -4,13 +4,25 @@ public class Drunkard {
 
     private static final int PARS_TOTAL_COUNT = Par.values().length; //9
     private static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length; //36
-
+    private static int[][] playersCards = new int[2][CARDS_TOTAL_COUNT];
+    private static int[] playersCardTails = new int[2];
+    private static int[] playersCardHeads = new int[2];
+    private static Integer[] allCards = new Integer[CARDS_TOTAL_COUNT];
     public static void main(String... __) {
 
 //        System.out.println("Масть 36-й карты - " + getSuit(35));
 //        System.out.println("Размерность 36-й карты - " + getPar(35));
-        System.out.println(toString(11));
+//        System.out.println(toString(11));
+    
+for (int i = 0; i < allCards.length; i++) {
+            allCards[i] = i + 1 ;
+            System.out.println(allCards[i] + " ");
+
+        }
+        
     }
+
+
     enum Suit {
         SPADES, // пики
         HEARTS, // червы
@@ -20,6 +32,7 @@ public class Drunkard {
     private static Suit getSuit(int cardNumber) {
         return Suit.values()[cardNumber / PARS_TOTAL_COUNT];
     }
+
     enum Par {
         SIX,
         SEVEN,
@@ -35,8 +48,20 @@ public class Drunkard {
     private static Par getPar(int cardNumber) {
         return Par.values()[cardNumber % PARS_TOTAL_COUNT];
     }
+
+    private static int incrementIndex(int i) {
+        return (i + 1) % CARDS_TOTAL_COUNT;
+    }
+
+    private static boolean playerCardsIsEmpty(int playerIndex) {
+        int tail = playersCardTails[playerIndex];
+        int head = playersCardHeads[playerIndex];
+        return tail == head;
+    }
+
     private static String toString(int cardNumber) {
         return getPar(cardNumber) + " " + getSuit(cardNumber);
     }
-}
 
+
+}
