fb7259c54b30d74440527405bc2a3955436e750b
Mikhail Kudrinskiy <m.kudrinskiy@gmail.com>
2019-03-16 00:21:01 +0300

    прогресс пятый шаг

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -8,95 +8,69 @@ import java.util.Collections;
 public class Drunkard {
     private static final int PARS_TOTAL_COUNT = Par.values().length; //9
     private static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length; //36
-    private static int[][] playersCards = new int[2][CARDS_TOTAL_COUNT];
+    private static int[][] playersCards = new int[2][CARDS_TOTAL_COUNT +1];
     private static int[] playersCardTails = new int[] {0, 0};
     private static int[] playersCardHeads = new int[] {18, 18};
     private static int[] allCards = new int[CARDS_TOTAL_COUNT];
-    private static int i = 0;
+    private static int count = 0;
+    private static int playerIndex;
 
     public static void main(String... __) {
-
-//        System.out.println("Масть 36-й карты - " + getSuit(35));
-//        System.out.println("Размерность 36-й карты - " + getPar(35));
-
 
         for (int i = 0; i < allCards.length; i++) {
             allCards[i] = i ;
         }
-        MathArrays.shuffle(allCards);
-        System.arraycopy(allCards, 0, playersCards[0], 0, 18);
-        System.arraycopy(allCards, 18, playersCards[1], 0, 18);
-        System.out.println(Arrays.toString(playersCards[0]));
-        System.out.println(Arrays.toString(playersCards[1]));
-       // playersCards[0][21] = 2;
-//        System.out.println(playersCards[0][0] + " " + toString(playersCards[0][0]));
-//        System.out.println(playersCards[1][0] + " " + toString(playersCards[1][0]));
-      //  System.out.println(Arrays.toString(playersCards[0]));
-      //  System.out.println(incrementIndex(1));
 
+        shuffleKoloda(allCards);
 
-        while (i < 18) {
+        do {
+            count = count + 1;
+            int card1 = (playersCards[0][playersCardTails[0]])%9;
+            int card2 = (playersCards[1][playersCardTails[1]])%9;
 
-            int karta1 = playersCards[0][playersCardTails[0]];
-            int karta2 = playersCards[1][playersCardTails[1]];
-            int a = karta1 % 9;
-            int b = karta2 % 9;
+            System.out.printf("Итерация №%d Игрок №1 карта: %s; Игрок №2 карта: %s%n", count, toString(card1),toString(card2));
 
-            System.out.println(playersCards[0][playersCardTails[0]] + " " + toString(playersCards[0][playersCardTails[0]]) + " " + a);
-            System.out.println(playersCards[1][playersCardTails[1]] + " " + toString(playersCards[1][playersCardTails[1]]) + " " + b);
+            if (card1 > card2) {
+                System.out.println("Выйграл Игрок №1!");
+                playerIndex = 0;
+                checkCard(playerIndex);
+                checkTail();
+                checkHead(playerIndex);
+                System.out.printf("У Игрока №1 %d, у игрока №2 %d карт %n", Cards(0), Cards(1));
 
-            int count = incrementIndex(i);
-            System.out.printf("Итерация №%d Игрок №1 карта: %s; Игрок №2 карта: %s\n", count, toString(karta1),toString(karta2));
+            } else if (card1 < card2) {
+                System.out.println("Выйграл Игрок №2!");
+                playerIndex = 1;
+                checkCard(playerIndex);
+                checkTail();
+                checkHead(playerIndex);
 
-//            System.out.println(36%9);
-//            System.out.println(b);
-            if (a > b) {
-                System.out.println("Выйграл Игрок №1!\n");
-
-                playersCards[0][playersCardHeads[0]] = playersCards[0][playersCardTails[0]];
-                playersCards[0][playersCardHeads[0]+1] = playersCards[1][playersCardTails[0]];
-
-                playersCardTails[0] = playersCardTails[0] + 1;
-                playersCardTails[1] = playersCardTails[1] + 1;
-
-                playersCardHeads[0] = playersCardHeads[0] + 2;
-
-
-
-
-            } else if (a < b) {
-                System.out.println("Выйграл Игрок №2!\n");
-
-                playersCards[1][playersCardHeads[1]] = playersCards[0][playersCardTails[0]];
-                playersCards[1][playersCardHeads[1]+1] = playersCards[1][playersCardTails[0]];
-
-                playersCardTails[0] = playersCardTails[0] + 1;
-                playersCardTails[1] = playersCardTails[1] + 1;
-
-                playersCardHeads[1] = playersCardHeads[1] + 2;
-
+                System.out.printf("У Игрока №1 %d карт, у игрока №2 %d карт %n", Cards(0), Cards(1));
             }
             else {
-                System.out.println("Ничья!Все остаются при своих!\n");
-                playersCardTails[0] = playersCardTails[0] + 1;
-                playersCardTails[1] = playersCardTails[1] + 1;
+                System.out.println("Ничья!Все остаются при своих!");
+
+                playersCards[0][playersCardHeads[0]] = playersCards[0][playersCardTails[0]];
+                playersCards[1][playersCardHeads[1]] = playersCards[1][playersCardTails[0]];
+                checkTail();
+                playersCardHeads[0] = incrementIndex(playersCardHeads[0]);
+                playersCardHeads[1] = incrementIndex(playersCardHeads[1]);
+
+                System.out.printf("У Игрока №1 %d, у игрока №2 %d карт %n", Cards(0), Cards(1));
+
             }
-
-            i++;
-
-            System.out.println(Arrays.toString(playersCards[0]));
-            System.out.println(Arrays.toString(playersCards[1]));
-        }
-
-
-
-
-
-
-
+            count++;
+        } while (!playerCardsIsEmpty(0) || !playerCardsIsEmpty(1));
 
     }
 
+    private static int [] shuffleKoloda (int[] arr) {
+
+        MathArrays.shuffle(arr);
+        System.arraycopy(arr, 0, playersCards[0], 0, CARDS_TOTAL_COUNT/2);
+        System.arraycopy(arr, CARDS_TOTAL_COUNT/2, playersCards[1], 0, CARDS_TOTAL_COUNT/2);
+        return arr;
+    }
 
 
     enum Suit {
@@ -143,9 +117,27 @@ public class Drunkard {
         return getPar(cardNumber) + " " + getSuit(cardNumber);
     }
 
-//    private static int winnerPlayer {
-//
-//    }
+    private static void checkTail () {
+        playersCardTails[0] = incrementIndex(playersCardTails[0]);
+        playersCardTails[1] = incrementIndex(playersCardTails[1]);
+
+    }
+
+    private static void checkHead (int playerIndex) {
+        playersCardHeads[playerIndex] = incrementIndex(playersCardHeads[playerIndex]+1);
+    }
+
+    private static void checkCard (int playerIndex) {
+        playersCards[playerIndex][playersCardHeads[playerIndex]] = playersCards[0][playersCardTails[0]];
+        playersCards[playerIndex][playersCardHeads[playerIndex]+1] = playersCards[1][playersCardTails[0]];
+    }
+
+    private static int Cards (int playerIndex) {
+        if (playersCardHeads[playerIndex] < playersCardTails[playerIndex])
+            return (CARDS_TOTAL_COUNT + playersCardHeads[playerIndex] - playersCardTails[playerIndex]);
+        else
+            return (playersCardHeads[playerIndex] - playersCardTails[playerIndex]);
+    }
 
 }
 
